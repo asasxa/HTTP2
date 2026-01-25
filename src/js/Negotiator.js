8eba118837319b7f1752ee2662ef438f794b9bc5
Ruslan Suleimanov <herwer.cool@gmail.com>
2026-01-25 10:37:53 +0300

    i

--- src/js/Negotiator.js
+++ src/js/Negotiator.js
@@ -8,58 +8,36 @@ export default class Negotiator {
       throw new Error('Параметр options функции createRequest не задан');
     }
 
-    const { method, data, callback, headers = {} } = options;
-    let url = this.baseURL;
-    if (options.url) {
-      url += options.url;
-    }
+    const { method, data, callback, url: requestUrl = '' } = options;
+    const fullUrl = this.baseURL + requestUrl;
 
     const xhr = new XMLHttpRequest();
-
     xhr.timeout = 10000;
-    xhr.onerror = () => {
-      console.error('Network error: request failed');
-    };
-    xhr.ontimeout = () => {
-      console.error('Request timeout');
-    };
+    xhr.onerror = () => console.error('Network error');
+    xhr.ontimeout = () => console.error('Request timeout');
 
-    try {
-      xhr.open(method, url);
+    xhr.open(method, fullUrl);
 
-      for (const key in headers) {
-        xhr.setRequestHeader(key, headers[key]);
-      }
-
-      xhr.onloadend = () => {
-        if (String(xhr.status).startsWith('2')) {
-          console.log('Сервер принял и обработал запрос.');
-          if (callback) {
-            try {
-              const response = xhr.responseText ? JSON.parse(xhr.responseText) : null;
-              callback(response);
-            } catch (e) {
-              callback(xhr.responseText);
-            }
-          }
-        } else {
-          let content = 'Сервер не принял запрос. ';
-          content += `Ошибка ${xhr.status}: ${xhr.statusText}.`;
-          console.error(content);
-        }
-      };
-
-      let sendData = data;
-      if (data !== undefined && typeof data === 'object' && !(data instanceof FormData)) {
-        sendData = JSON.stringify(data);
-        if (!headers['Content-Type'] && !headers['content-type']) {
-          xhr.setRequestHeader('Content-Type', 'application/json');
-        }
-      }
-
-      xhr.send(sendData);
-    } catch (e) {
-      console.error(e);
+    let sendData = data;
+    if (data !== undefined && typeof data === 'object' && !(data instanceof FormData)) {
+      sendData = JSON.stringify(data);
+      xhr.setRequestHeader('Content-Type', 'application/json');
     }
+
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        let response = null;
+        try {
+          response = xhr.responseText ? JSON.parse(xhr.responseText) : null;
+        } catch (e) {
+          response = xhr.responseText;
+        }
+        if (callback) callback(response);
+      } else {
+        console.error(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+      }
+    };
+
+    xhr.send(sendData);
   }
 }
